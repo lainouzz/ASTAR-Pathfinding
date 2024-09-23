100520e7649a7770094c775cba2bba0ffbb13d53
Linus Lind <linus.lind@edu.futuregames.nu>
2024-09-23 15:13:22 +0200

    small fix

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public Transform car;
+    public LayerMask HitLayer;
 
-
+    public float rayDistance;
+    
+    private Vector3 distance;
+    
+    private RaycastHit hit;
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 distance = car.transform.position - Camera.main.transform.position;
+        distance = car.transform.position - Camera.main.transform.position;
         
-        RaycastHit hit;
-        if (Physics.Raycast(distance, Vector3.up, out hit, Mathf.Infinity))
+        //CarCheck(hit);
+        ObstacleCheck(hit);
+    }
+
+    void CarCheck(RaycastHit hit)
+    {
+        if (Physics.Raycast(distance, Vector3.up, out hit, rayDistance))
         {
-            Debug.DrawRay(Camera.main.transform.position, distance * hit.distance, Color.yellow);
-            Debug.Log("hit something");
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.DrawRay(Camera.main.transform.position, distance * 1, Color.yellow);
+                Debug.Log("hit something");
+            }
+        }
+    }
+
+    void ObstacleCheck(RaycastHit hit)
+    {
+        if (Physics.Raycast(distance, Vector3.up, out hit, rayDistance, HitLayer))
+        {
+            Debug.DrawRay(Camera.main.transform.position, distance * 1, Color.red);
+            Debug.Log("Wall hit: " + gameObject.name); 
         }
     }
 }
